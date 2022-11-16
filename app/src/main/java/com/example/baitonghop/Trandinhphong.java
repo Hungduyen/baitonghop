2b17f0ca224e90afb91906aa4d6c722fa9b78347
DIEN MAY XANH <nguyentronghung3005@gmail.com>
2022-11-16 08:42:50 +0700

    Add new project food

---
+++ app/src/main/java/com/example/baitonghop/Trandinhphong.java
@@ -0,0 +1,14 @@
+package com.example.baitonghop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Trandinhphong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trandinhphong);
+    }
+}
